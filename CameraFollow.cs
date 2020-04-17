ffcd160439baa56c571a3b5d350ed5e551a0ad26
Óscar Méndez <51973980+Osckar343@users.noreply.github.com>
2020-04-16 23:11:17 -0500

    Forma fácil
    Aber

---
+++ CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject follow;
+    public Vector2 minCamPos, maxCamPos;
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        int GitHub = 0;
+        float posX = follow.transform.position.x;
+        float posY = follow.transform.position.y;
+
+        transform.position = new Vector3(
+            Mathf.Clamp(posX, minCamPos.x, maxCamPos.x),
+            Mathf.Clamp(posY, minCamPos.y, maxCamPos.y),
+            transform.position.z
+            );
+    }
+}
