c1ec0e27dde30b18774e554e00b30bd7def7fe1a
Óscar Méndez <oscarandres131@hotmail.com>
2020-04-19 14:09:24 -0500

    Montañas de Fondo (Parallax)
    Se han agregado 3 sprites de montañas.
    Se ha agregado 1 Script (Parallax Effect).
    Las sprites de montañas están en capas negativas (Order in Layer -1)
    
    Se implemetó elementos visuales en el fondo simulando efecto de paralaje.

---
+++ Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+
+    public GameObject follow;
+    private float posXOriginal;
+    private float posYOriginal;
+
+    public float speedX;
+    public float speedY;
+
+    public bool ejeX = true;
+    public bool ejeY = true;
+    
+
+    void Start()
+    {
+        posXOriginal = transform.position.x;
+        posYOriginal = transform.position.y;
+    }
+
+    void Update()
+    {
+        /*Mueve la posición de la cámara*/
+        float posX = follow.transform.position.x;
+        float posY = follow.transform.position.y;
+
+
+
+        if(ejeX == true && ejeY == true)
+        {
+            transform.position = new Vector3(posX + posXOriginal + (posX * speedX), posY + posYOriginal + (posY * speedY), transform.position.z);
+        }
+
+        if(ejeX == true && ejeY == false)
+        {
+            transform.position = new Vector3(posX + posXOriginal + (posX * speedX), posYOriginal, transform.position.z);
+        }
+
+        if (ejeX == false && ejeY == true)
+        {
+            transform.position = new Vector3(posXOriginal, posY + posYOriginal + (posY * speedY), transform.position.z);
+        }
+
+
+
+
+    }
+}
