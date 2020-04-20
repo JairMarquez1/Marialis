8c48e4c73b4c30b68661c7a9b55b6d8b54b8a790
Óscar Méndez <oscarandres131@hotmail.com>
2020-04-20 01:12:26 -0500

    Commit Corregido (Estrellas y Montañas)
    Ahora el juego tiene los objetos implementados en la escena.

---
+++ Assets/Scripts/StarsEffect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsEffect : MonoBehaviour
+{
+    public GameObject follow;
+    public float speedRotation = 0.1f;
+    //public Light dayTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //dayTime.intensity = transform.rotation.z;
+
+        /*Mueve la posición de la cámara*/
+        float posX = follow.transform.position.x;
+        float posY = follow.transform.position.y;
+
+        transform.position = new Vector3(posX, posY -4f, transform.position.z);
+        transform.Rotate(new Vector3(0, 0, 0.1f) * speedRotation);   
+    }
+}
