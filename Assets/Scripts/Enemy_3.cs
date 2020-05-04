a4026b95ab4d3633511774587addbf27534e29ed
Óscar Méndez <oscarandres131@hotmail.com>
2020-05-04 01:28:43 -0500

    0.05 Campo de Pruebas.
    -Nueva escena añadida para hacer pruebas (Testing Ground).
    -Enemy3 (En desarrollo) añadido.
    -----------------------
    Añadido:
    
    -Script Enemy_3.
    -Script PhysicsRockLaunch.
    
    -Prefab Rock

---
+++ Assets/Scripts/Enemy_3.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_3 : MonoBehaviour
+{
+    public GameObject rock;
+    public Transform follow;
+    
+    void Start()
+    {
+        InvokeRepeating("LaunchProjectile", 2, 1);
+    }
+
+    void FixedUpdate()
+    {
+        float angle = Angle(follow.transform.position, transform.position) - 90f;
+        angle = Mathf.Clamp(angle, -90f, 110f);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, angle);
+    }
+
+
+
+    void LaunchProjectile()
+    {
+        Instantiate(rock, transform.position, transform.rotation);
+    }
+
+
+    /*Calcula el ángulo entre 2 puntos*/
+    static float Angle(Vector3 P1, Vector3 P2) 
+    {
+        Vector3 vector = P1 - P2; 
+        vector.Normalize(); //Convierte los valores en base a 1.
+
+        float angle = Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg;
+
+        return angle;
+    }
+}
---
+++ Assets/Scripts/PhysicsRockLaunch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsRockLaunch : MonoBehaviour
+{
+    public Rigidbody2D rock;
+    public float forceX;
+    public float forceY;
+
+    void Start()
+    {
+       rock.AddForce(transform.up * 8f,ForceMode2D.Impulse);
+    }
+
+}
