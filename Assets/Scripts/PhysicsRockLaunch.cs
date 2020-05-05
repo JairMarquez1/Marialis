5ddc821d97737aed0de1379c3526fa128e1bd87f
JairMarquez1 <a19310186@ceti.mx>
2020-05-05 00:19:55 -0500

    0.05.3
    Commit maldito

--- Assets/Scripts/PhysicsRockLaunch.cs
+++ Assets/Scripts/PhysicsRockLaunch.cs
@@ -7,10 +7,15 @@ public class PhysicsRockLaunch : MonoBehaviour
     public Rigidbody2D rock;
     public float forceX;
     public float forceY;
+    public float rockLife = 5f;
 
     void Start()
     {
        rock.AddForce(transform.up * 8f,ForceMode2D.Impulse);
     }
 
+    void Update()
+    {
+        Destroy(gameObject, rockLife);
+    }
 }
