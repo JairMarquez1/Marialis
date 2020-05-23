429c0db5b7785c9b15f4f5fc2c5811c1bd7f3527
Óscar Méndez <oscarandres131@hotmail.com>
2020-05-23 01:20:26 -0500

    0.09.2 Mostrar/Ocultar medidor de combustible JetPack.
    Ahora el medidor se mostrará o se ocultará dependiendo si el jugador tiene el JetPack activado.
    (HasJetPack del Script "Movement")
    
    ---Modificaciones---
    Se agregó una condicional if/else donde si el jugador tiene el JetPack se mostrará el medidor en el script "FuelMeterView".

--- Assets/Scripts/FuelMeterView.cs
+++ Assets/Scripts/FuelMeterView.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 
 public class FuelMeterView : MonoBehaviour
 {
-    public Image fuelView;
+    public Image baseView; 
+    public Image iconView;
+    public Image fuelView; 
+
     public Movement player;
     private float maxFuel = 100f;
 
@@ -14,7 +17,24 @@ public class FuelMeterView : MonoBehaviour
     void FixedUpdate()
     {
         float fuel = player.fuelJetpack;
-        fuel = Mathf.Clamp(fuel, 0, 100);
-        fuelView.transform.localScale = new Vector2(1, fuel/maxFuel);
+        bool hasJetPack = player.hasJetPack;
+
+        fuel = Mathf.Clamp(fuel, 0, 100); 
+        fuelView.transform.localScale = new Vector2(1, fuel/maxFuel); //Barra de combustible. (Escala de 0 a 1)
+
+
+        /*Muestra el medidor de combustible si el jugador tiene (o no) el JetPack*/
+        if (hasJetPack)
+        {
+            baseView.enabled = true;
+            fuelView.enabled = true;
+            iconView.enabled = true;
+        }
+        else
+        {
+            baseView.enabled = false;
+            fuelView.enabled = false;
+            iconView.enabled = false;
+        }                    
     }
 }
