ccf57c926c24838607049318782da07da3785fcb
JairMarquez1 <a19310186@ceti.mx>
2020-07-20 01:33:55 -0500

    0.24.2
    merge

---
+++ Assets/Scripts/Text/Dialogue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable] //Hace visible las oraciones en el editor.
+public class Dialogue
+{
+    public string name;
+
+    [TextArea(3,10)] //Fija el tamaño para escribir las oraciones en el editor.
+    public string[] sentenceList;
+}
