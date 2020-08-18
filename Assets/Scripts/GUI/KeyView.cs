482924ffcdc86c92bb3b7ed84601b0f3b8a903cb
Óscar Méndez <51973980+Osckar343@users.noreply.github.com>
2020-08-17 22:32:03 -0500

    0.27.0 Implementación de TileMap en Escena Tutorial.
    Se implementó tilemap en la escena Tutorial. (Estado en desarrollo).
    
    Se cambiaron los sprites de las teclas del Canvas a Sprites.
    
    Se agregó una fuente de texto para los monitores controlados por la IA.

--- Assets/Scripts/GUI/KeyView.cs
+++ Assets/Scripts/GUI/KeyView.cs
@@ -5,38 +5,90 @@ using UnityEngine;
 
 public class KeyView : MonoBehaviour
 {
+    public GameObject playerPosition;
+    public GameObject keyboardPosition;
+
     /*Teclas*/
-    public Image iconView_X;
-    public Image iconView_Z;
-    public Image iconView_Right;
-    public Image iconView_Left;
-    public Image iconView_Down;
-    public Image iconView_Up;
+    public Renderer X_key;
+    public Renderer Z_key;
+    public Renderer Right_key;
+    public Renderer Left_key;
+    public Renderer Down_key;
+    public Renderer Up_key;
 
     /*Interacciones*/
     public triggerTutorial sides;
     public triggerTutorial sneak;
-    //public triggerTutorial...
+    public triggerTutorial jump;
 
     public bool pressKeyToMoveSides;
     public bool pressKeyToSneak;
+    public bool pressKeyToJump;
 
     private void Start()
     {
-        iconView_Down.enabled = false;
-        iconView_Left.enabled = false;
-        iconView_Right.enabled = false;
-        iconView_Up.enabled = false;
-        iconView_X.enabled = false;
-        iconView_Z.enabled = false;
+        pressKeyToMoveSides = false;
+        pressKeyToSneak = false;
     }
 
 
     void FixedUpdate()
     {
+        positioningKeysOnPlayer();
+
         PressKeyToMoveToTheSides();
         PressKeyToSneak();
+        PressKeyToJump();
+        
         //...
+    }
+
+    private void positioningKeysOnPlayer()
+    {
+        float x_Keyboard = playerPosition.transform.position.x;
+        float y_Keyboard = playerPosition.transform.position.y + 1.5f;
+
+        keyboardPosition.transform.position = new Vector2(x_Keyboard,y_Keyboard);
+    }
+    private void pressThisKey(Renderer key) //Resalta el color de la tecla para indicarle al jugador que debe presionarla.
+    {
+        key.material.color = Color.white;
+        key.gameObject.GetComponent<Animator>().enabled = true;
+    }
+    private void ignoreThisKey(Renderer key) //Opaca el color para darle menos importancia a la tecla.
+    {
+        key.material.color = new Color(0.7f, 0.7f, 0.7f, 1); //Más oscuro (negro).
+        key.gameObject.GetComponent<Animator>().enabled = false;
+    }
+    private void showArrowKeys(bool arrowKeys)
+    {
+        if (arrowKeys == true)
+        {
+            Up_key.enabled = true;
+            Down_key.enabled = true;
+            Right_key.enabled = true;
+            Left_key.enabled= true;
+        }
+        else
+        {
+            Up_key.enabled = false;
+            Down_key.enabled = false;
+            Right_key.enabled = false;
+            Left_key.enabled = false;
+        }
+    }
+    private void showActionKeys(bool actionKeys)
+    {
+        if(actionKeys == true)
+        {
+            X_key.enabled = true;
+            Z_key.enabled= true;
+        }
+        else
+        {
+            X_key.enabled = false;
+            Z_key.enabled = false;
+        }
     }
 
     private void PressKeyToMoveToTheSides()
@@ -45,25 +97,60 @@ public class KeyView : MonoBehaviour
 
         if (pressKeyToMoveSides == true)
         {
-            iconView_Right.enabled = true;
-            iconView_Left.enabled = true;
+            showArrowKeys(true);
+
+            pressThisKey(Left_key);
+            pressThisKey(Right_key);
+
+            ignoreThisKey(Up_key);
+            ignoreThisKey(Down_key);
         }
         else
         {
-            iconView_Right.enabled = false;
-            iconView_Left.enabled = false;
+            showArrowKeys(false);
         }
     }
-
     private void PressKeyToSneak()
     {
         pressKeyToSneak = sneak.isInside;
 
         if (pressKeyToSneak == true)
-            iconView_Down.enabled = true;       
+        {
+            showArrowKeys(true);
+
+            pressThisKey(Down_key);
+
+            ignoreThisKey(Right_key);     
+            ignoreThisKey(Left_key);
+            ignoreThisKey(Up_key);
+        }
         else
-            iconView_Down.enabled = false;
+        {
+            showActionKeys(false);
+        }
+             
         
+    }
+
+    private void PressKeyToJump()
+    {
+        pressKeyToJump = jump.isInside;
+
+        if (pressKeyToJump == true)
+        {
+            showArrowKeys(true);
+
+            pressThisKey(Up_key);
+            
+
+            ignoreThisKey(Right_key);
+            ignoreThisKey(Left_key);
+            ignoreThisKey(Down_key);
+        }
+        else
+        {
+            showActionKeys(false);
+        }
     }
 }
 
