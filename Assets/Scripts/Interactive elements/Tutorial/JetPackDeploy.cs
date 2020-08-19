5bbc6af34f70e7d16afe0576996e067213d9079d
Óscar Méndez <51973980+Osckar343@users.noreply.github.com>
2020-08-18 21:53:13 -0500

    0.27.1 Tutorial - Presionar Tecla para tomar objeto.
    -Indicador de presionar tecla cuando estás sobre un accesorio.
    
    -Cambio de voz de la IA Sujeta a cambios).
    
    -Implementos de interacción en el tutorial.

---
+++ Assets/Scripts/Interactive elements/Tutorial/JetPackDeploy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetPackDeploy : MonoBehaviour
+{
+    public GameObject jetPackPrefab;
+    public triggerTutorial collision;
+    private bool firstTimeInside;
+
+    private void Start()
+    {
+        firstTimeInside = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && firstTimeInside == true)
+        {
+            float x = transform.position.x - 4f;
+            float y = transform.position.y + 9f;
+
+            GameObject jetPack = Instantiate(jetPackPrefab, new Vector2(x, y), transform.rotation);
+            jetPack.name = "jetPack";
+
+            firstTimeInside = false;
+        }
+            
+    }
+
+}
